6ec235edb0726057f18760cb782d224fe51952bb
jotasugiyama <c0117165b3@edu.teu.ac.jp>
2020-03-05 19:37:41 +0900

    [fix] コンフリクト修正

--- index/js/index.js
+++ index/js/index.js
@@ -9,10 +9,5 @@ new Vue({
         axios
         .get('https://www.googleapis.com/books/v1/volumes?q=ソフトウェアファースト')
         .then(response => (this.items = response.data.items))
-<<<<<<< HEAD
     }
 })
-=======
-    },
-});
->>>>>>> a24320f2d13101fa985a5498774eb0240e05a06b
